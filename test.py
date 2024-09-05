79f4dce851f27a0aabacc341df2a2b1936f517ee
kcenceis <25601736a@gmail.com>
2024-09-06 02:34:57 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,48 +1,61 @@
+from DrissionPage import ChromiumPage, ChromiumOptions
 import re
 
-import mistune
-import requests
-from bs4 import BeautifulSoup
+class nyaa_list:
+    address = ''
+    title = ''
+    torrent = ''
+    magnet = ''
+    category = ''
+    file_name = ''
+    Information = ''
+    Submitter = ''
+    Comments = ''
+    count = 0
 
-import Utils
-from module import xpic
+co = ChromiumOptions()
+co.incognito()  # 无痕模式
+co.headless()  # 无头模式
+# 设置UA
+co.set_user_agent(
+    user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0')
+co.set_argument('--no-sandbox')
+co.set_argument('--window-size', '800,600')
+co.set_argument('--start-maximized')
+co.set_argument('--guest')
+co.set_argument("--disable-gpu")
+# co.set_proxy("http://10.1.2.253:2000")
+page = ChromiumPage(co)
 
-headers = {}
-headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
-                        "Chrome/114.0.0.0 Safari/537.36"
-headers['Content-Type'] = "application/x-www-form-urlencoded"
-headers['sec-ch-ua'] = '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"'
-mReq = requests.session()
-mReq.headers = headers
-https_pattern = '(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Za-z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Za-z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Za-z0-9+&@#/%=~_|$?!:,.]*\)|[A-Za-z0-9+&@#/%=~_|$])'
+page.get("https://sukebei.nyaa.si/view/4170491")
+nyaa_list = nyaa_list()
 
-
-r = mReq.get("https://sukebei.nyaa.si/view/4025043")
-mSoup = BeautifulSoup(r.text, 'html.parser')
-for stringSoup in mSoup.find_all('div',class_='panel-body'):
-    #print(len(stringSoup))
-    for i in stringSoup.find_all('div',class_='row'):
-        if re.search('Information:',str(i)):
-            print(i.find('a',rel='noopener noreferrer nofollow').text)
-
-for stringSoup in mSoup.find_all('div', id='torrent-description'):
-    b = stringSoup.string  # 获取网页文中字段
-    # b转换为str
-    b = str(b)
-    b = mistune.html(b)
-    print(b)
-    url = re.findall(https_pattern, b)
-    print(url)
-    for b in url:
-        print("将要进行抓取的网址:{}".format(b))  # 抓取到的地址 将要进行抓取的网址
-        str_b = str(b)
-        if re.search('http[s]?://[\w\W]{0,7}postimg.cc/', str_b):
-            #xpic.getImageURL(str_b)
-            print(str_b)
-            r = mReq.get(str_b)
-            soup = BeautifulSoup(r.text, 'html.parser')
-            #mReq.headers['referer'] = re.findall('http[s]?://[\w\W]{0,100}/', str(url))
-            print(soup)
-            #for k in soup.find_all('a', id='download'):
-            #    print(k)
-#print(r.text)
+ddd = page.eles("tag:div@class=panel-footer clearfix")
+download_pattern = re.compile(r'/download/(?:[0-9])+.torrent')  # 种子pattern
+magnet_pattern = re.compile(r'magnet:\?xt=urn:btih:')  # 磁链pattern
+# 获取种子链接 磁链
+for i in ddd:
+    tag_a = i.eles('tag:a')
+    for x in tag_a:
+        href = x.attr('href')
+        if re.search(download_pattern, href):
+            nyaa_list.torrent = href
+        if re.search(magnet_pattern, href):
+            nyaa_list.magnet = href
+# 获取标题 目录
+for i in page.eles('tag:div@class=panel panel-default'):
+    for k in i.eles('tag:h3@class=panel-title'):
+        nyaa_list.title = k.text
+# for i in page.eles('tag:div@class=panel panel-default'):
+category = page.eles('tag:div@class=col-md-5')[0].eles('tag:a')[1].text
+nyaa_list.category = category
+for i in page.eles("tag:div@class=row"):
+    # for x in i.eles("tag:div@class=row"):
+    #    print(x.html)
+    if re.search("Submitter:", i.html):
+        for y in i.eles("tag:a@class=text-success"):
+            nyaa_list.Submitter = y.text
+    elif re.search("Information:", i.html):
+        Information = [y.text for y in i.eles("tag:div@class=col-md-5")][0]
+        nyaa_list.Information = Information
+Utils.process_url(page, nyaa_list)

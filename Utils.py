c6538bbbe9628271e47932c4dc0dde51498a350e
kcenceis <25601736a@gmail.com>
2024-11-04 20:41:34 +0800

    fix bugs

--- Utils.py
+++ Utils.py
@@ -104,6 +104,11 @@ def getRequest(http_url):
     # r.raise_for_status()
     return r
 
+# 防止插入相同内容
+def add_unique_element(lst, element):
+    if element not in lst:
+        lst.append(element)
+
 # 将不能作为文件名的字符替换为下划线
 def validateTitle(title):
     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
@@ -115,21 +120,12 @@ def downimg(a,src,nyaa_list):
     nyaa_list.file_name = filename_encode(src)
     a.save(path=nyaa_list.Path, name=nyaa_list.file_name)
     SQLUtils.insertSQL_file_history(nyaa_list, src)
-
-# 防止插入相同内容
-def add_unique_element(lst, element):
-    if element not in lst:
-        lst.append(element)
-
 # 连接并获取网页内容（第二页 即/view/111XXXX）
 # 传入nyaa_list
 def down(nyaa_list):
     co = ChromiumOptions()
     co.incognito()  # 无痕模式
     co.headless()  # 无头模式
-    co.set_load_mode('eager') # 加载html后停止
-    co.no_js(True) #不加载js
-    co.no_imgs(True) # 不加载图片
     # 设置UA
     co.set_user_agent(
         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0')
@@ -138,12 +134,12 @@ def down(nyaa_list):
     co.set_argument('--start-maximized')
     co.set_argument('--guest')
     co.set_argument("--disable-gpu")
-    #co.auto_port(True)
+    co.auto_port(True)
     # co.set_proxy("http://10.1.2.253:2000")
     page = ChromiumPage(co)
     print("将要进行抓取的网址:{}".format(nyaa_list.address))
+    page.get(nyaa_list.address)
     try:
-        page.get(nyaa_list.address)
         for i in page.eles("tag:div@class=row"):
             # for x in i.eles("tag:div@class=row"):
             #    print(x.html)
@@ -155,161 +151,155 @@ def down(nyaa_list):
                 Information = [y.text for y in i.eles("tag:div@class=col-md-5")][0]
                 nyaa_list.Information = Information
         SQLUtils.updateSQL_http_history_information(nyaa_list) # 写入Submitter information comments信息到数据
-        torrent_text = page.ele('tag:div@id=torrent-description')
-        print(nyaa_list.Information)
-        tag_img=torrent_text.eles('tag:img')
-        print(tag_img)
-        for i in tag_img:
-            print(i)
-        #tag_img = [item.eles('tag:img') for item in torrent_text]
+        torrent_text = page.eles('tag:div@id=torrent-description')
+        tag_img = [item.eles('tag:img') for item in torrent_text]
         htmlurl_list = []
-        #print(torrent_text)
-        html_url = []
-        # 防止插入相同内容URL
-        for i in re.findall(https_pattern, torrent_text.html):
-            add_unique_element(html_url, i)
-        #print(html_url)
-    #    if len(tag_img)>0:
-    #        for i in tag_img[0]:
-    #            src = i.attr('src')
-    #            print("将要进行匹配的网址:{}".format(src))
-    #            if re.search('^http[s]?://[\w\W]{0,2}hentai\.org/.*$', src):
-    #                downimg(i,src,nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,2}\.wp\.com/.*$', src):
-    #                downimg(i,src,nyaa_list)
-    #            elif re.search('^http[s]?://i.imgur.com/.*.[jpg|bmp|png|jpeg|webp|gif]$', src):
-    #                downimg(i,src,nyaa_list)
-    #    html_url = [re.findall(https_pattern, item.html) for item in torrent_text]
-    #    if len(html_url) > 0:
-    #        #print(html_url)
-    #        for i in html_url[0]:
-    #            if i not in htmlurl_list:
-    #               htmlurl_list.append(i)
-    #        for b in htmlurl_list:
-    #            print("将要进行抓取的网址:{}".format(b))  # 抓取到的地址 将要进行抓取的网址
-    #            str_b = str(b)
-    #            # 文件名定义
-    #            # 只获取https://hentai-covers.site开头的网址
-    #            if re.search('https://hentai-covers.site', str_b):
-    #                hentaicovers.getImageURL(b, nyaa_list)
-    #            # 只获取https://hentai4free.net开头的网址
-    #            # b=图片url,mBookTitle[mCount]=图片标题,count=第几张图片,len(url)=url总数
-    #            elif re.search('https://hentai4free.net', str_b):
-    #                hentai4free.getImageURL(b, nyaa_list)
-    #            # 只获取https://imagetwist.com开头的网址
-    #            elif re.search('https://imagetwist.com', str_b):
-    #                imagetwist.getImageURL(b, nyaa_list)
-    #            # 防止获取到缩略图 抓取5-11个字符之间的链接
-    #            elif re.search('^http[s]?://imgfrost.net/\w{5,11}$', str_b):
-    #                imgfrost.getImageURL(b, nyaa_list, "0")
-    #            elif re.search('^http[s]?://imgblaze.net/\w{5,11}$', str_b):
-    #                imgfrost.getImageURL(b, nyaa_list, "1")
-    #            elif re.search('^http[s]?://ibb.co/.+?$', str_b):
-    #                ibb.get_image(b, nyaa_list)
-    #            # imgtaxi.com imgadult.com
-    #            # small 改成 big 类的图站
-    #            elif re.search('^http[s]?://imgtaxi.com/.*.html$', str_b):
-    #                imgtaxi.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://imgadult.com/.*.html$', str_b):
-    #                imgtaxi.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://imgdrive.net/.*.html$', str_b):
-    #                imgtaxi.get_image(b, nyaa_list)
-    #            # silverpic.com imgbaron.com pics4you.net picdollar.com premalo.com
-    #            elif re.search('^https://silverpic.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://imgbaron.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            # 中间匹配 数字+字母+下划线 10次以上 最后贪婪匹配所有 结尾.html
-    #            # elif re.search('^http[s]?://pics4you.net/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #            elif re.search('^http[s]?://pics4you.net/\w{10,}/[\u0000-\uFFFF]+.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://picdollar.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://premalo.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://fotokiz.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://imgsen.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://imgsto.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://imgstar.eu/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-    #            elif re.search('^http[s]?://fotokiz.com/.*.html$', str_b):
-    #                silverpic.get_image(b, nyaa_list)
-#
-    #            elif re.search('^http[s]?://ehgt.org.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-#
-    #            # ukkit
-    #            #  elif re.search('^http[s]?://skviap.xyz/v/.*$', str_b):
-    #            #      ukkit.get_image(b,nyaa_list)
-    #            #  elif re.search('^http[s]?://bvmqkla.de/v/.*$', str_b):
-    #            #      ukkit.get_image(b, nyaa_list)
-    #            #  elif re.search('^http[s]?://doddbt.com/v/.*$', str_b):
-    #            #      ukkit.get_image(b, nyaa_list)
-#
-    #            elif re.search('^http[s]?://skviap.xyz.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://bvmqkla.de.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://doddbt.com.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://img.dlsite.jp/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('imagebam.com', str_b):
-    #                download_img(b, nyaa_list)
-    #            #elif re.search('^http[s]?://[\w\W]{0,2}imgur\.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #            #    download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,7}caching\.ovh/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,7}turboimg\.net/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            # 2022/5/5新增
-    #            #elif re.search('http[s]?://pixxxels.cc/', str_b):
-    #            #    pixxxels.getImageURL(b, nyaa_list)
-    #            #elif re.search('http[s]?://postimg.cc/', str_b):
-    #            #    pixxxels.getImageURL(b, nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,7}ax21pics.net/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            # 2022/05/07
-    #            elif re.search('^http[s]?://[\w\W]{0,7}ckvwpzp.xyz/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,7}imgxx.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,12}imageshack.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://pics.dmm.co.jp/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            # 2023/06/09
-    #            # croea,imagehaha,imagexport为同个网页模板,服务端跟imagetwist有关系
-    #            elif re.search('^http[s]?://[\w\W]{0,7}catbox.moe/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                download_img(b, nyaa_list)
-    #            # 可能会卡死 不抓取diogo4d
-    #            # elif re.search('^http[s]?://diogo4d.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #            #    download_img(b, nyaa_list)
-    #            elif re.search('^http[s]?://imagehaha.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                imagehaha(str_b, nyaa_list)
-    #            elif re.search('^http[s]?://croea.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                croea(str_b, nyaa_list)
-    #            elif re.search('^http[s]?://imagexport.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                croea(str_b, nyaa_list)
-    #            # 2024/1/15
-    #            # 添加xpic支持
-    #            elif re.search('http[s]?://xpic.org/', str_b):
-    #                xpic.getImageURL(str_b, nyaa_list)
-    #            # 添加postimg直连图片下载
-    #            elif re.search('http[s]?://i.postimg.cc/', str_b):
-    #                download_img(b, nyaa_list)
-#
-#
-    #            elif re.search('^http[s]?://[\w\W]{0,2}hentai\.org/.*$', str_b):
-    #                page.download()
-    #                downimg(str_b,nyaa_list)
-    #            elif re.search('^http[s]?://[\w\W]{0,2}\.wp\.com/.*$', str_b):
-    #                page.download()
-    #            elif re.search('^http[s]?://i.imgur.com/.*.[jpg|bmp|png|jpeg|webp|gif]$', str_b):
-    #                page.download()
-    #            # 不在抓取范围,结束抓取并记录
+        if len(tag_img)>0:
+            for i in tag_img[0]:
+                src = i.attr('src')
+                print("将要进行匹配的网址:{}".format(src))
+                if re.search('^http[s]?://[\w\W]{0,2}hentai\.org/.*$', src):
+                    downimg(i,src,nyaa_list)
+                elif re.search('^http[s]?://[\w\W]{0,2}\.wp\.com/.*$', src):
+                    downimg(i,src,nyaa_list)
+                elif re.search('^http[s]?://i.imgur.com/.*.[jpg|bmp|png|jpeg|webp|gif]$', src):
+                    downimg(i,src,nyaa_list)
+        html_url = [re.findall(https_pattern, item.html) for item in torrent_text]
+        if len(html_url) > 0:
+            #print(html_url)
+            for i in html_url[0]:
+                if i not in htmlurl_list:
+                   htmlurl_list.append(i)
+            for b in htmlurl_list:
+                print("将要进行抓取的网址:{}".format(b))  # 抓取到的地址 将要进行抓取的网址
+                str_b = str(b)
+                # 文件名定义
+
+                # 只获取https://hentai-covers.site开头的网址
+                if re.search('https://hentai-covers.site', str_b):
+                    hentaicovers.getImageURL(b, nyaa_list)
+                # 只获取https://hentai4free.net开头的网址
+                # b=图片url,mBookTitle[mCount]=图片标题,count=第几张图片,len(url)=url总数
+                elif re.search('https://hentai4free.net', str_b):
+                    hentai4free.getImageURL(b, nyaa_list)
+                # 只获取https://imagetwist.com开头的网址
+                elif re.search('https://imagetwist.com', str_b):
+                    imagetwist.getImageURL(b, nyaa_list)
+
+                # 防止获取到缩略图 抓取5-11个字符之间的链接
+                elif re.search('^http[s]?://imgfrost.net/\w{5,11}$', str_b):
+                    imgfrost.getImageURL(b, nyaa_list, "0")
+                elif re.search('^http[s]?://imgblaze.net/\w{5,11}$', str_b):
+                    imgfrost.getImageURL(b, nyaa_list, "1")
+
+                elif re.search('^http[s]?://ibb.co/.+?$', str_b):
+                    ibb.get_image(b, nyaa_list)
+
+                # imgtaxi.com imgadult.com
+                # small 改成 big 类的图站
+                elif re.search('^http[s]?://imgtaxi.com/.*.html$', str_b):
+                    imgtaxi.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://imgadult.com/.*.html$', str_b):
+                    imgtaxi.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://imgdrive.net/.*.html$', str_b):
+                    imgtaxi.get_image(b, nyaa_list)
+
+                # silverpic.com imgbaron.com pics4you.net picdollar.com premalo.com
+                elif re.search('^https://silverpic.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://imgbaron.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                # 中间匹配 数字+字母+下划线 10次以上 最后贪婪匹配所有 结尾.html
+                # elif re.search('^http[s]?://pics4you.net/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                elif re.search('^http[s]?://pics4you.net/\w{10,}/[\u0000-\uFFFF]+.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://picdollar.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://premalo.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://fotokiz.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://imgsen.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://imgsto.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://imgstar.eu/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+                elif re.search('^http[s]?://fotokiz.com/.*.html$', str_b):
+                    silverpic.get_image(b, nyaa_list)
+
+
+                elif re.search('^http[s]?://ehgt.org.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+
+
+                # ukkit
+                #  elif re.search('^http[s]?://skviap.xyz/v/.*$', str_b):
+                #      ukkit.get_image(b,nyaa_list)
+                #  elif re.search('^http[s]?://bvmqkla.de/v/.*$', str_b):
+                #      ukkit.get_image(b, nyaa_list)
+                #  elif re.search('^http[s]?://doddbt.com/v/.*$', str_b):
+                #      ukkit.get_image(b, nyaa_list)
+
+                elif re.search('^http[s]?://skviap.xyz.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://bvmqkla.de.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://doddbt.com.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+
+                elif re.search('^http[s]?://img.dlsite.jp/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                elif re.search('imagebam.com', str_b):
+                    download_img(b, nyaa_list)
+                #elif re.search('^http[s]?://[\w\W]{0,2}imgur\.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                #    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://[\w\W]{0,7}caching\.ovh/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+
+                elif re.search('^http[s]?://[\w\W]{0,7}turboimg\.net/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+
+                # 2022/5/5新增
+                #elif re.search('http[s]?://pixxxels.cc/', str_b):
+                #    pixxxels.getImageURL(b, nyaa_list)
+                #elif re.search('http[s]?://postimg.cc/', str_b):
+                #    pixxxels.getImageURL(b, nyaa_list)
+
+                elif re.search('^http[s]?://[\w\W]{0,7}ax21pics.net/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+
+                # 2022/05/07
+                elif re.search('^http[s]?://[\w\W]{0,7}ckvwpzp.xyz/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://[\w\W]{0,7}imgxx.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://[\w\W]{0,12}imageshack.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://pics.dmm.co.jp/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+
+                # 2023/06/09
+                # croea,imagehaha,imagexport为同个网页模板,服务端跟imagetwist有关系
+                elif re.search('^http[s]?://[\w\W]{0,7}catbox.moe/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    download_img(b, nyaa_list)
+                # 可能会卡死 不抓取diogo4d
+                # elif re.search('^http[s]?://diogo4d.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                #    download_img(b, nyaa_list)
+                elif re.search('^http[s]?://imagehaha.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    imagehaha(str_b, nyaa_list)
+                elif re.search('^http[s]?://croea.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    croea(str_b, nyaa_list)
+                elif re.search('^http[s]?://imagexport.com/.*[jpg|bmp|png|jpeg|webp|gif]$', str_b):
+                    croea(str_b, nyaa_list)
+
+                # 2024/1/15
+                # 添加xpic支持
+                elif re.search('http[s]?://xpic.org/', str_b):
+                    xpic.getImageURL(str_b, nyaa_list)
+                # 添加postimg直连图片下载
+                elif re.search('http[s]?://i.postimg.cc/', str_b):
+                    download_img(b, nyaa_list)
+                # 不在抓取范围,结束抓取并记录
     finally:
         page.quit(force=True)
